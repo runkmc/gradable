c104ab0c056fd2d570f5a4d9b1a2b52b32612e6f
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-02-20 12:44:40 -0800

    Add Classroom class

---
+++ Gradable/Classroom.swift
@@ -0,0 +1,14 @@
+//
+//  Classroom.swift
+//  Gradable
+//
+//  Created by Kevin McGladdery on 2/20/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+
+final class Classroom {
+    @NSManaged private(set) var title: String
+    @NSManaged private(set) var notes: String
+}
