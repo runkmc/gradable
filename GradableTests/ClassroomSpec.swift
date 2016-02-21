228b17f05ab86885180f75f9be092532e4876c7a
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-02-21 15:53:48 -0800

    Create ClassroomSpec file

---
+++ GradableTests/ClassroomSpec.swift
@@ -0,0 +1,17 @@
+//
+//  ClassroomSpec.swift
+//  Gradable
+//
+//  Created by Kevin McGladdery on 2/21/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+
+class ClassroomSpec: QuickSpec {
+    override func spec() {
+        
+    }
+}
