5acbf05965643c556e126b7b7172ad7e3c6c721a
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-02-22 15:33:26 -0800

    Add setupCoreData file

---
+++ Gradable/setupCoreData.swift
@@ -0,0 +1,10 @@
+//
+//  setupCoreData.swift
+//  Gradable
+//
+//  Created by Kevin McGladdery on 2/22/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import CoreData
