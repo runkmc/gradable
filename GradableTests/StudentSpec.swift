6a94f5982dea09bebde28be2ab21dd0805dcecc3
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-02-23 14:20:37 -0800

    Student has properties

--- GradableTests/StudentSpec.swift
+++ GradableTests/StudentSpec.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import CoreData
+import Quick
+import Nimble
+@testable import Gradable
+
+class StudentSpec: QuickSpec {
+    static let moc = setupCoreData(NSInMemoryStoreType)
+    
+    override func spec() {
+        describe("a Student") {
+            let room = NSEntityDescription.insertNewObjectForEntityForName("Classroom", inManagedObjectContext: StudentSpec.moc) as! Classroom
+            room.setValue("MUS1001", forKey: "title")
+            room.setValue("Music 1345, TTh 9 - 9:50", forKey: "notes")
+            let student = NSEntityDescription.insertNewObjectForEntityForName("Student", inManagedObjectContext: StudentSpec.moc) as! Student
+            student.setValue("Benjamin", forKey: "firstName")
+            student.setValue("Sisko", forKey: "lastName")
+            student.setValue(room, forKey: "classroom")
+            
+            it("has properties") {
+                expect(student.firstName) == "Benjamin"
+                expect(student.lastName) == "Sisko"
+                expect(student.classroom) == room
+            }
+        }
+    }
+}
