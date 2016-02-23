513c0e30b8e37697f561ebdc963127eb9c5faf6f
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-02-22 18:11:04 -0800

    Classroom has notes and title properties

--- GradableTests/ClassroomSpec.swift
+++ GradableTests/ClassroomSpec.swift
@@ -7,15 +7,25 @@
 //
 
 import Foundation
+import CoreData
 import Quick
 import Nimble
 @testable import Gradable
 
 class ClassroomSpec: QuickSpec {
+    static let moc = setupCoreData(NSInMemoryStoreType)
+    
     override func spec() {
         describe("a Classroom") {
+            let room = NSEntityDescription.insertNewObjectForEntityForName("Classroom", inManagedObjectContext: ClassroomSpec.moc) as! Classroom
+            room.setValue("MUS1001", forKey: "title")
+            room.setValue("Music 1345, TTh 9 - 9:50", forKey: "notes")
+            try! ClassroomSpec.moc.save()
+            
             it("has properties") {
-                
+                print(room.notes)
+                expect(room.title) == "MUS1001"
+                expect(room.notes) == "Music 1345, TTh 9 - 9:50"
             }
         }
     }
