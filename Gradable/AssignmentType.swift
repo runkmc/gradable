19d62c672a6f4b61a797ae1db360603da9f6c4d8
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-02-24 13:14:35 -0800

    Create AssignmentType class

---
+++ Gradable/AssignmentType.swift
@@ -0,0 +1,15 @@
+//
+//  AssignmentType.swift
+//  Gradable
+//
+//  Created by Kevin McGladdery on 2/24/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class AssignmentType: NSManagedObject {
+    @NSManaged var name: String
+    @NSManaged var percentage: Int16
+}
