06a8d8b0e798b80e0c2c959fdfc4c7939c608d7b
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-02-27 16:05:02 -0800

    Add Assignment Class

---
+++ Gradable/Assignment.swift
@@ -0,0 +1,16 @@
+//
+//  Assignment.swift
+//  Gradable
+//
+//  Created by Kevin McGladdery on 2/27/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+final class Assignment: NSManagedObject {
+    @NSManaged var assignedDate: NSDate?
+    @NSManaged var dueDate: NSDate?
+    @NSManaged var name: String
+}
