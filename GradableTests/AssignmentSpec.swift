c0876aaf7902cf94f53a50b4789dabf885d40541
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-03-10 12:51:34 -0800

    AssignmentType has a name

--- GradableTests/AssignmentSpec.swift
+++ GradableTests/AssignmentSpec.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import CoreData
+import Nimble
+import Quick
+@testable import Gradable
+
+class AssignmentSpec: QuickSpec {
+    static let moc = setupCoreData(NSInMemoryStoreType)
+    
+    override func spec() {
+        describe("An assignment type") {
+            let room = NSEntityDescription.insertNewObjectForEntityForName("Gradebook", inManagedObjectContext: AssignmentSpec.moc) as! Gradebook
+            room.setValue("MUS1001", forKey: "title")
+            room.setValue("Music 1345, TTh 9 - 9:50", forKey: "notes")
+            let aType = NSEntityDescription.insertNewObjectForEntityForName("AssignmentType", inManagedObjectContext: AssignmentSpec.moc) as! AssignmentType
+            aType.name = "Quiz"
+            aType.percentage = 20
+            aType.gradebook = room
+            try! AssignmentTypeSpec.moc.save()
+            
+            it("has properties") {
+                expect(aType.name) == "Quiz"
+            }
+            
+        }
+    }
+}
