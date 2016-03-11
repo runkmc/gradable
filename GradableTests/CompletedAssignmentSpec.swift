edab2022e130e5d3785d641ae61b1af3f15c8f03
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-03-11 11:28:48 -0800

    Create CompletedAssignmentSpec file

---
+++ GradableTests/CompletedAssignmentSpec.swift
@@ -0,0 +1,9 @@
+//
+//  CompletedAssignmentSpec.swift
+//  Gradable
+//
+//  Created by Kevin McGladdery on 3/11/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
