a82e6c7918e1c312f6b1b7f524fc26808267134f
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-03-17 13:43:47 -0700

    Create GradebookViewModelSpec

---
+++ GradableTests/GradebookViewModelSpec.swift
@@ -0,0 +1,16 @@
+//
+//  GradebookViewModelSpec.swift
+//  Gradable
+//
+//  Created by Kevin McGladdery on 3/17/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import Quick
+import Nimble
+@testable import Gradable
+
+class GradebookViewModelSpec: QuickSpec {
+}
